ad1870c40836bb3f346882ac33f826e84a461b6e
Akash <akash.munagala@gmail.com>
2026-01-21 22:42:38 -0500

    Return track 2 equivalent data even if checkmark fails

--- nfc/main/main.cpp
+++ nfc/main/main.cpp
@@ -4,8 +4,8 @@
 #include "Slice.h"
 #include "errors.h"
 
-#include <PN532_SPI.h>
 #include <PN532.h>
+#include <PN532_SPI.h>
 #include <tlv.h>
 
 #include <cstdint>
@@ -90,21 +90,126 @@ std::optional<ReadSlice> exchangeDataICT(const char *pre,
   printHex(pre, toSend);
 
   uint8_t recvLen = recvBuf.size();
-  CHECK_PRINT_RETURN_OPT("Failed to inCommunicateThru", nfc.inCommunicateThru(toSend.data(), toSend.size(), recvBuf.data(), &recvLen));
+  CHECK_PRINT_RETURN_OPT("Failed to inCommunicateThru",
+                         nfc.inCommunicateThru(toSend.data(), toSend.size(),
+                                               recvBuf.data(), &recvLen));
 
   ReadSlice readSlice(recvBuf.data(), recvLen);
   return readSlice;
 }
 
 bool exchangeDataICT(std::span<const uint8_t> toSend) {
-  CHECK_PRINT_RETURN_BOOL("Failed to inCommunicateThru", nfc.inCommunicateThru(toSend.data(), toSend.size()));
+  CHECK_PRINT_RETURN_BOOL("Failed to inCommunicateThru",
+                          nfc.inCommunicateThru(toSend.data(), toSend.size()));
 
   return false;
 }
 
+bool tryCheckmark(TLVS &tlvs, WriteSlice &writeSlice, std::span<uint8_t> rbuf,
+                  WriteSlice &track2Slice) {
+  std::optional<ReadSlice> readSliceOpt;
+  ReadSlice readSlice{nullptr, 0};
+  // Read records
+  TLVNode *aflNode = tlvs.findTLV(0x94);
+  CHECK_PRINT_RETURN_BOOL("No AFL data found", aflNode);
+  static uint8_t aflBuf[AFL_BUFSIZE];
+  size_t aflLen = aflNode->getValueLength();
+  CHECK_PRINT_RETURN_BOOL(
+      "aflBuf is not large enough - bufLen: %zu - requiredlen: %zu",
+      aflLen <= AFL_BUFSIZE, AFL_BUFSIZE, aflLen);
+  memcpy(aflBuf, aflNode->getValue(), aflLen);
+  ReadSlice aflSlice{aflBuf, aflLen};
+  tlvs.reset();
+
+  // Figure out the last sent block number
+  readSliceOpt = exchangeDataICT("R(NACK): ", {{0xB2}}, rbuf);
+  CHECK_RETURN_BOOL(readSliceOpt);
+  readSlice = *readSliceOpt;
+  uint8_t blockNum = ((readSlice.readByte() & 0xA0) != 0) ? 0x1 : 0x0;
+  Serial.printf("blockNum: %02x\n", blockNum);
+
+  static uint8_t cdolBuf[CDOL_BUFSIZ];
+  static WriteSlice cdolSlice(cdolBuf, sizeof(cdolBuf));
+  cdolSlice.reset();
+
+  while (aflSlice.len() > 0) {
+    uint8_t sfi = aflSlice.readByte();
+    uint8_t recordToRead = aflSlice.readByte();
+    uint8_t endRecord = aflSlice.readByte();
+    // number of records included in data authentication
+    uint8_t _ = aflSlice.readByte();
+    for (; recordToRead <= endRecord; ++recordToRead) {
+      Serial.printf("Reading record %02x\n", recordToRead);
+      static uint8_t recordbuf[RECORD_BUFSIZ];
+      WriteSlice recordSlice(recordbuf, RECORD_BUFSIZ);
+
+      writeSlice.reset();
+      blockNum ^= 1;
+      uint8_t pcb = 0x2 | blockNum;
+      uint8_t p2 = (sfi & 0xF8) | 0x4;
+      readSliceOpt = exchangeDataICT(
+          "Read Record: ", {{pcb, 0x00, 0xB2, recordToRead, p2, 0x00}}, rbuf);
+      while (true) {
+        CHECK_RETURN_BOOL(readSliceOpt);
+        readSlice = *readSliceOpt;
+
+        uint8_t responsePcb = readSlice.readByte();
+        CHECK_PRINT_RETURN_BOOL("Failed to buffer record data",
+                                recordSlice.append(readSlice.span()));
+
+        // If no more data, break
+        if ((responsePcb & 0x10) == 0) {
+          break;
+        }
+        // Send R(ACK) to get more data
+        writeSlice.reset();
+        blockNum ^= 1;
+        pcb = 0xA2 | blockNum;
+        readSliceOpt = exchangeDataICT("R(ACK): ", {{pcb}}, rbuf);
+      }
+      printHex("Full Read Record response: ", recordSlice.span());
+      tlvs.decodeTLVs(recordbuf, recordSlice.len());
+
+      if (TLVNode *cdolNode = tlvs.findTLV(0x8C)) {
+        CHECK_PRINT_RETURN_BOOL("Found duplicate CDOL tags",
+                                cdolSlice.len() == 0);
+        cdolSlice.append({cdolNode->getValue(), cdolNode->getValueLength()});
+      }
+      if (TLVNode *track2Node = tlvs.findTLV(TRACK2_TAG)) {
+        CHECK_PRINT_RETURN_BOOL("Found duplicate Track 2 tags",
+                                track2Slice.len() == 0);
+        track2Slice.append(
+            {track2Node->getValue(), track2Node->getValueLength()});
+      }
+      tlvs.reset();
+    }
+  }
+
+  if (cdolSlice.len() == 0) {
+    // If there's no CDOL, then there's nothing else we can do, and we have to
+    // just return whatever track 2 data we've found
+    CHECK_PRINT_RETURN_BOOL("No CDOL or Track 2 Equivalent Data found",
+                            track2Slice.len());
+    return true;
+  }
+  printHex("CDOL data: ", cdolSlice.span());
+  writeSlice.reset();
+  bool builtAC = writeSlice.appendApduCommand(
+      0x80, 0xAE, 0x50, 0x00, [](WriteSlice &slice) {
+        ReadSlice cdolReadSlice{cdolSlice.data(), cdolSlice.len()};
+        return slice.appendFromDol(cdolReadSlice);
+      });
+  CHECK_PRINT_RETURN_BOOL("Failed to build GENERATE AC command", builtAC);
+
+  readSliceOpt = exchangeData("Sending GENERATE AC: ", writeSlice.span(), rbuf);
+  CHECK_PRINT_RETURN_BOOL("Failed to do GENERATE AC", readSliceOpt);
+
+  return true;
+}
+
 std::optional<std::span<const uint8_t>> getTrack2Data() {
   bool foundCard = nfc.inListPassiveTarget();
-  
+
   if (foundCard) {
     Serial.println("\nFound something!");
     std::optional<ReadSlice> readSliceOpt;
@@ -172,103 +277,11 @@ std::optional<std::span<const uint8_t>> getTrack2Data() {
           {track2Node->getValue(), track2Node->getValueLength()});
     }
 
-    // Read records
-    TLVNode *aflNode = tlvs.findTLV(0x94);
-    CHECK_PRINT_RETURN_OPT("No AFL data found", aflNode);
-    static uint8_t aflBuf[AFL_BUFSIZE];
-    size_t aflLen = aflNode->getValueLength();
-    CHECK_PRINT_RETURN_OPT(
-        "aflBuf is not large enough - bufLen: %zu - requiredlen: %zu",
-        aflLen <= AFL_BUFSIZE, AFL_BUFSIZE, aflLen);
-    memcpy(aflBuf, aflNode->getValue(), aflLen);
-    ReadSlice aflSlice{aflBuf, aflLen};
-    tlvs.reset();
-
-    // Figure out the last sent block number
-    readSliceOpt = exchangeDataICT("R(NACK): ", {{0xB2}}, rbuf);
-    CHECK_RETURN_OPT(readSliceOpt);
-    readSlice = *readSliceOpt;
-    uint8_t blockNum = ((readSlice.readByte() & 0xA0) != 0) ? 0x1 : 0x0;
-    Serial.printf("blockNum: %02x\n", blockNum);
-
-    static uint8_t cdolBuf[CDOL_BUFSIZ];
-    static WriteSlice cdolSlice(cdolBuf, sizeof(cdolBuf));
-    cdolSlice.reset();
-
-    while (aflSlice.len() > 0) {
-      uint8_t sfi = aflSlice.readByte();
-      uint8_t recordToRead = aflSlice.readByte();
-      uint8_t endRecord = aflSlice.readByte();
-      // number of records included in data authentication
-      uint8_t _ = aflSlice.readByte();
-      for (; recordToRead <= endRecord; ++recordToRead) {
-        Serial.printf("Reading record %02x\n", recordToRead);
-        static uint8_t recordbuf[RECORD_BUFSIZ];
-        WriteSlice recordSlice(recordbuf, RECORD_BUFSIZ);
-
-        writeSlice.reset();
-        blockNum ^= 1;
-        uint8_t pcb = 0x2 | blockNum;
-        uint8_t p2 = (sfi & 0xF8) | 0x4;
-        readSliceOpt = exchangeDataICT(
-            "Read Record: ", {{pcb, 0x00, 0xB2, recordToRead, p2, 0x00}}, rbuf);
-        while (true) {
-          CHECK_RETURN_OPT(readSliceOpt);
-          readSlice = *readSliceOpt;
-
-          uint8_t responsePcb = readSlice.readByte();
-          CHECK_PRINT_RETURN_OPT("Failed to buffer record data",
-                                 recordSlice.append(readSlice.span()));
-
-          // If no more data, break
-          if ((responsePcb & 0x10) == 0) {
-            break;
-          }
-          // Send R(ACK) to get more data
-          writeSlice.reset();
-          blockNum ^= 1;
-          pcb = 0xA2 | blockNum;
-          readSliceOpt = exchangeDataICT("R(ACK): ", {{pcb}}, rbuf);
-        }
-        printHex("Full Read Record response: ", recordSlice.span());
-        tlvs.decodeTLVs(recordbuf, recordSlice.len());
-
-        if (TLVNode *cdolNode = tlvs.findTLV(0x8C)) {
-          CHECK_PRINT_RETURN_OPT("Found duplicate CDOL tags",
-                                 cdolSlice.len() == 0);
-          cdolSlice.append({cdolNode->getValue(), cdolNode->getValueLength()});
-        }
-        if (TLVNode *track2Node = tlvs.findTLV(TRACK2_TAG)) {
-          CHECK_PRINT_RETURN_OPT("Found duplicate Track 2 tags",
-                                 track2Slice.len() == 0);
-          track2Slice.append(
-              {track2Node->getValue(), track2Node->getValueLength()});
-        }
-        tlvs.reset();
-      }
+    // FIX
+    if (!tryCheckmark(tlvs, writeSlice, rbuf, track2Slice)) {
+      Serial.println("Failed to get checkmark, but might still have Track 2 "
+                     "Equivalent Data");
     }
-
-    if (cdolSlice.len() == 0) {
-      // If there's no CDOL, then there's nothing else we can do, and we have to
-      // just return whatever track 2 data we've found
-      CHECK_PRINT_RETURN_OPT("No CDOL or Track 2 Equivalent Data found",
-                             track2Slice.len());
-      return track2Slice.span();
-    }
-    CHECK_PRINT_RETURN_VAL("No CDOL found", cdolSlice.len(),
-                           track2Slice.span());
-    printHex("CDOL data: ", cdolSlice.span());
-    writeSlice.reset();
-    bool builtAC = writeSlice.appendApduCommand(
-        0x80, 0xAE, 0x50, 0x00, [](WriteSlice &slice) {
-          ReadSlice cdolReadSlice{cdolSlice.data(), cdolSlice.len()};
-          return slice.appendFromDol(cdolReadSlice);
-        });
-    CHECK_PRINT_RETURN_OPT("Failed to build GENERATE AC command", builtAC);
-
-    readSliceOpt =
-        exchangeData("Sending GENERATE AC: ", writeSlice.span(), rbuf);
-    CHECK_PRINT_RETURN_OPT("Failed to send ", readSliceOpt);
 
     CHECK_PRINT_RETURN_OPT(
         "No Track 2 Equivalent Data found after reading records",
@@ -277,12 +290,12 @@ std::optional<std::span<const uint8_t>> getTrack2Data() {
   } else {
     // Set CIU_BitFraming register to send 8 bits
     uint16_t addr = 0x633D;
-    CHECK_PRINT_RETURN_OPT(
-        "Failed to set CIU_BitFraming for ECP",
-        nfc.writeRegister(addr, 0x00));
+    CHECK_PRINT_RETURN_OPT("Failed to set CIU_BitFraming for ECP",
+                           nfc.writeRegister(addr, 0x00));
 
     // Send the ECP frame
-    exchangeDataICT({{0x6a, 0x02, 0xc8, 0x01, 0x00, 0x03, 0x00, 0x06, 0x7f, 0x01, 0x00, 0x00, 0x00, 0x4d, 0xef}});
+    exchangeDataICT({{0x6a, 0x02, 0xc8, 0x01, 0x00, 0x03, 0x00, 0x06, 0x7f,
+                      0x01, 0x00, 0x00, 0x00, 0x4d, 0xef}});
   }
 
   return std::nullopt;
