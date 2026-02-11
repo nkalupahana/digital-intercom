a1917d55b460d50cfe2b0d49fdae9c9abae9e708
nkalupahana <avishkank@gmail.com>
2026-02-10 19:50:15 -0500

    Nisala refactor

--- nfc/main/card.h
+++ nfc/main/card.h
@@ -2,18 +2,30 @@
 
 #include "Slice.h"
 #include "errors.h"
+#include "nfc.h"
 #include "utils.h"
+#include <cstdint>
 #include <optional>
 #include <span>
-#include <cstdint>
 #include <tlv.h>
-#include "nfc.h"
 
 constexpr size_t RECORD_BUFSIZ = 300;
 constexpr size_t CDOL_BUFSIZ = 64;
 constexpr size_t AFL_BUFSIZE = 64;
 constexpr size_t TRACK2_TAG = 0x57;
 constexpr size_t TRACK2_TAG_BUFSIZ = 19;
+
+bool sendECPFrame() {
+  // Set CIU_BitFraming register to send 8 bits
+  uint16_t addr = 0x633D;
+  CHECK_PRINT_RETURN_BOOL("Failed to set CIU_BitFraming for ECP",
+                          nfc.writeRegister(addr, 0x00));
+
+  // Send frame
+  exchangeDataICT({{0x6a, 0x02, 0xc8, 0x01, 0x00, 0x03, 0x00, 0x06, 0x7f, 0x01,
+                    0x00, 0x00, 0x00, 0x4d, 0xef}});
+  return true;
+}
 
 bool tryCheckmark(TLVS &tlvs, WriteSlice &writeSlice, std::span<uint8_t> rbuf,
                   WriteSlice &track2Slice) {
@@ -128,96 +140,80 @@ bool tryCheckmark(TLVS &tlvs, WriteSlice &writeSlice, std::span<uint8_t> rbuf,
   return true;
 }
 
+bool ppseSelect() {
+    
+}
+
 std::optional<std::span<const uint8_t>> getTrack2Data() {
-  bool foundCard = nfc.inListPassiveTarget();
+  std::optional<ReadSlice> readSliceOpt;
+  ReadSlice readSlice{nullptr, 0};
+  static TLVS tlvs;
+  static uint8_t rbuf[PN532_PACKBUFFSIZ];
+  static uint8_t sbuf[PN532_PACKBUFFSIZ];
+  WriteSlice writeSlice(sbuf, PN532_PACKBUFFSIZ);
 
-  if (foundCard) {
-    Serial.println("\nFound something!");
-    std::optional<ReadSlice> readSliceOpt;
-    ReadSlice readSlice{nullptr, 0};
-    static TLVS tlvs;
-    static uint8_t rbuf[PN532_PACKBUFFSIZ];
-    static uint8_t sbuf[PN532_PACKBUFFSIZ];
-    WriteSlice writeSlice(sbuf, PN532_PACKBUFFSIZ);
+  // SELECT PPSE
+  writeSlice.reset();
+  CHECK_RETURN_OPT(
+      writeSlice.appendApduCommand(0x00, 0xA4, 0x04, 0x00, "2PAY.SYS.DDF01"));
+  readSliceOpt = exchangeData("Sending SELECT PPSE: ", writeSlice.span(), rbuf);
+  CHECK_RETURN_OPT(readSliceOpt);
+  readSlice = *readSliceOpt;
 
-    // SELECT PPSE
-    writeSlice.reset();
-    CHECK_RETURN_OPT(
-        writeSlice.appendApduCommand(0x00, 0xA4, 0x04, 0x00, "2PAY.SYS.DDF01"));
-    readSliceOpt =
-        exchangeData("Sending SELECT PPSE: ", writeSlice.span(), rbuf);
-    CHECK_RETURN_OPT(readSliceOpt);
-    readSlice = *readSliceOpt;
+  // SELECT AID
+  tlvs.decodeTLVs(rbuf, readSlice.len());
+  TLVNode *aidNode = tlvs.findTLV(0x4F);
+  CHECK_PRINT_RETURN_OPT("Failed to get AID from card!", aidNode);
+  std::span<const uint8_t> aid{aidNode->getValue(), aidNode->getValueLength()};
+  tlvs.reset();
 
-    // SELECT AID
-    tlvs.decodeTLVs(rbuf, readSlice.len());
-    TLVNode *aidNode = tlvs.findTLV(0x4F);
-    CHECK_PRINT_RETURN_OPT("Failed to get AID from card!", aidNode);
-    std::span<const uint8_t> aid{aidNode->getValue(),
-                                 aidNode->getValueLength()};
+  writeSlice.reset();
+  CHECK_RETURN_OPT(writeSlice.appendApduCommand(0x00, 0xA4, 0x04, 0x00, aid));
+  readSliceOpt = exchangeData("Sending SELECT AID: ", writeSlice.span(), rbuf);
+  CHECK_RETURN_OPT(readSliceOpt);
+  readSlice = *readSliceOpt;
+
+  // GPO
+  tlvs.decodeTLVs(readSlice.data(), readSlice.len());
+  TLVNode *pdolNode = tlvs.findTLV(0x9F38);
+
+  writeSlice.reset();
+  if (pdolNode == nullptr) {
     tlvs.reset();
-
-    writeSlice.reset();
-    CHECK_RETURN_OPT(writeSlice.appendApduCommand(0x00, 0xA4, 0x04, 0x00, aid));
-    readSliceOpt =
-        exchangeData("Sending SELECT AID: ", writeSlice.span(), rbuf);
-    CHECK_RETURN_OPT(readSliceOpt);
-    readSlice = *readSliceOpt;
-
-    // GPO
-    tlvs.decodeTLVs(readSlice.data(), readSlice.len());
-    TLVNode *pdolNode = tlvs.findTLV(0x9F38);
-
-    writeSlice.reset();
-    if (pdolNode == nullptr) {
-      tlvs.reset();
-      Serial.println("Failed to find PDOL. Using empty DOL");
-      CHECK_RETURN_OPT(
-          writeSlice.appendApduCommand(0x80, 0xA8, 0x00, 0x00, {{0x83, 0x00}}));
-    } else {
-      ReadSlice pdol{pdolNode->getValue(), pdolNode->getValueLength()};
-      tlvs.reset();
-      CHECK_RETURN_OPT(writeSlice.appendApduCommand(
-          0x80, 0xA8, 0x00, 0x00, [&pdol](WriteSlice &slice) {
-            return slice.appendTLV(0x83, [&pdol](WriteSlice &slice) {
-              return slice.appendFromDol(pdol);
-            });
-          }));
-    }
-    readSliceOpt = exchangeData("Sending GPO: ", writeSlice.span(), rbuf);
-    CHECK_RETURN_OPT(readSliceOpt);
-    readSlice = *readSliceOpt;
-
-    // Try to get Track 2 data it it's already available
-    static uint8_t track2Buf[TRACK2_TAG_BUFSIZ];
-    WriteSlice track2Slice{track2Buf, sizeof(track2Buf)};
-    tlvs.decodeTLVs(readSlice.data(), readSlice.len());
-    TLVNode *track2Node = tlvs.findTLV(TRACK2_TAG);
-    if (track2Node) {
-      track2Slice.append(
-          {track2Node->getValue(), track2Node->getValueLength()});
-    }
-
-    // FIX
-    if (!tryCheckmark(tlvs, writeSlice, rbuf, track2Slice)) {
-      Serial.println("Failed to get checkmark, but might still have Track 2 "
-                     "Equivalent Data");
-    }
-
-    CHECK_PRINT_RETURN_OPT(
-        "No Track 2 Equivalent Data found after reading records",
-        track2Slice.len());
-    return track2Slice.span();
+    Serial.println("Failed to find PDOL. Using empty DOL");
+    CHECK_RETURN_OPT(
+        writeSlice.appendApduCommand(0x80, 0xA8, 0x00, 0x00, {{0x83, 0x00}}));
   } else {
-    // Set CIU_BitFraming register to send 8 bits
-    uint16_t addr = 0x633D;
-    CHECK_PRINT_RETURN_OPT("Failed to set CIU_BitFraming for ECP",
-                           nfc.writeRegister(addr, 0x00));
+    ReadSlice pdol{pdolNode->getValue(), pdolNode->getValueLength()};
+    tlvs.reset();
+    CHECK_RETURN_OPT(writeSlice.appendApduCommand(
+        0x80, 0xA8, 0x00, 0x00, [&pdol](WriteSlice &slice) {
+          return slice.appendTLV(0x83, [&pdol](WriteSlice &slice) {
+            return slice.appendFromDol(pdol);
+          });
+        }));
+  }
+  readSliceOpt = exchangeData("Sending GPO: ", writeSlice.span(), rbuf);
+  CHECK_RETURN_OPT(readSliceOpt);
+  readSlice = *readSliceOpt;
 
-    // Send the ECP frame
-    exchangeDataICT({{0x6a, 0x02, 0xc8, 0x01, 0x00, 0x03, 0x00, 0x06, 0x7f,
-                      0x01, 0x00, 0x00, 0x00, 0x4d, 0xef}});
+  // Try to get Track 2 data it it's already available
+  static uint8_t track2Buf[TRACK2_TAG_BUFSIZ];
+  WriteSlice track2Slice{track2Buf, sizeof(track2Buf)};
+  tlvs.decodeTLVs(readSlice.data(), readSlice.len());
+  TLVNode *track2Node = tlvs.findTLV(TRACK2_TAG);
+  if (track2Node) {
+    track2Slice.append({track2Node->getValue(), track2Node->getValueLength()});
   }
 
-  return std::nullopt;
+  // FIX
+  if (!tryCheckmark(tlvs, writeSlice, rbuf, track2Slice)) {
+    Serial.println("Failed to get checkmark, but might still have Track 2 "
+                   "Equivalent Data");
+  }
+
+  CHECK_PRINT_RETURN_OPT(
+      "No Track 2 Equivalent Data found after reading records",
+      track2Slice.len());
+  return track2Slice.span();
 }
