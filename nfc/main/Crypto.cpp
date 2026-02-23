099153ae23ba3350bb71257bbfa07b5e1f96571d
Akash <akash.munagala@gmail.com>
2026-02-22 21:39:30 -0500

    Add error handling for decrypt set key

--- nfc/main/Crypto.cpp
+++ nfc/main/Crypto.cpp
@@ -155,9 +155,11 @@ decryptResponse(std::span<const uint8_t> encrypted, WriteSlice &outputSlice) {
 
   std::span<const uint8_t, TAG_SIZE> tag = encrypted.last<TAG_SIZE>();
   size_t encryptedLen = dataLen - TAG_SIZE;
-  mbedtls_gcm_setkey(&gcmCtx, MBEDTLS_CIPHER_ID_AES, deviceKey, 256);
-
   uint8_t *outputPtr = outputSlice.deferAppend(encryptedLen);
+
+  CHECK_CRYPTO_RETURN_OPT(
+      "Failed to set key",
+      mbedtls_gcm_setkey(&gcmCtx, MBEDTLS_CIPHER_ID_AES, deviceKey, 256));
   CHECK_CRYPTO_RETURN_OPT(
       "Failed to decrypt",
       mbedtls_gcm_auth_decrypt(&gcmCtx, encryptedLen, iv, sizeof(iv), nullptr,
